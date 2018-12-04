8f5835d78e44859ffa96144242edcb78b51198d6
Jessie Renton <jsr.renton@gmail.com>
2018-12-04 16:06:52 +0000

    commented run file to be an example of running a single simulation

--- run.py
+++ run.py
@@ -1,21 +1,23 @@
 import numpy as np
-import libs.pd_lib as lib
+import libs.pd_lib as lib #library for simulation routines
 import libs.data as data
-import libs.plot as vplt
+import libs.plot as vplt #plotting library
 from structure.global_constants import *
 import structure.initialisation as init
 from structure.cell import Tissue, BasicSpringForceNoGrowth
 
-N = 4
-timend = 10
-timestep = 1.
+"""run a single voronoi tessellation model simulation"""
 
-rand = np.random.RandomState(42)
-b,c,DELTA = 10.,1.0,0.025
+l = 10 # population size N=l*l
+timend = 20 # simulation time (hours)
+timestep = 1. # time intervals to save simulation history
 
-simulation = lib.simulation_decoupled_update
-game = lib.prisoners_dilemma_averaged
-game_constants = (b,c)
+rand = np.random.RandomState()
+b,c,DELTA = 10.,1.0,0.0 #prisoner's dilemma game parameters
 
-history = lib.run_simulation(simulation,N,timestep,timend,rand,DELTA,game,game_constants)
+simulation = lib.simulation_decoupled_update  #simulation routine imported from lib
+game = lib.prisoners_dilemma_averaged #game imported from lib
+game_parameters = (b,c)
+
+history = lib.run_simulation(simulation,l,timestep,timend,rand,DELTA,game,game_parameters)
                 

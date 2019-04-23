ac8d105e482dcc28e58d5cb092afeb806eaa1dd0
Jessie Renton <jsr.renton@gmail.com>
2019-04-23 10:58:20 +0100

    include init_time as argument

--- libs/pd_lib.py
+++ libs/pd_lib.py
@@ -172,7 +172,7 @@ def simulation_no_division(tissue,dt,N_steps,rand):
 #         yield tissue
 
     
-def run_simulation(simulation,N,timestep,timend,rand,DELTA,game,constants,til_fix=True,save_areas=False,tissue=None,mutant_num=1):
+def run_simulation(simulation,N,timestep,timend,rand,DELTA,game,constants,init_time=10.,til_fix=True,save_areas=False,tissue=None,mutant_num=1):
     """initialise tissue with NxN cells and run given simulation with given game and constants.
             starts with single cooperator
             ends at time=timend OR if til_fix=True when population all cooperators (type=1) or defectors (2)

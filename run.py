7f54d54bdd76d4859f5242898e69edf649c5c23d
Jessie Renton <jsr.renton@gmail.com>
2019-04-26 13:53:40 +0100

    edit run files

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import numpy as np
-import libs.pd_lib_density as lib #library for simulation routines
+import libs.pd_lib as lib #library for simulation routines
 import libs.data as data
 import libs.plot as vplt #plotting library
 from structure.global_constants import *
@@ -8,18 +8,18 @@ from structure.cell import Tissue, BasicSpringForceNoGrowth
 
 """run a single voronoi tessellation model simulation"""
 
-l = 6 # population size N=l*l
-timend = 100 # simulation time (hours)
-timestep = 0.05 # time intervals to save simulation history
+l = 10 # population size N=l*l
+timend = 10 # simulation time (hours)
+timestep = 1. # time intervals to save simulation history
 
 rand = np.random.RandomState()
 b,c,DELTA = 3.,1.0,0.025 #prisoner's dilemma game parameters
-OMEGA = 1.
+OMEGA=1.
 
-simulation = lib.simulation_pd_density_dep  #simulation routine imported from lib
+simulation = lib.simulation_decoupled_update  #simulation routine imported from lib
 game = lib.prisoners_dilemma_averaged #game imported from lib
 game_parameters = (b,c)
 
 
-history = lib.run_simulation(simulation,l,timestep,timend,rand,DELTA,OMEGA,game,game_parameters,til_fix=False,mutant_num=1)
+history = lib.run_simulation(simulation,l,timestep,timend,rand,DELTA,game,game_parameters,til_fix=False,init_time=None,save_areas=True)
                 
--- run_neutral.py
+++ run_neutral.py
@@ -9,12 +9,11 @@ from structure.cell import Tissue, BasicSpringForceNoGrowth
 """run a single voronoi tessellation model simulation"""
 
 l = 10 # population size N=l*l
-timend = 50 # simulation time (hours)
-timestep = 0.1 # time intervals to save simulation history
+timend = 20 # simulation time (hours)
+timestep = 1.0 # time intervals to save simulation history
 
 rand = np.random.RandomState()
 
 simulation = lib.simulation_ancestor_tracking  #simulation routine imported from lib
 
-history = lib.run_simulation(simulation,l,timestep,timend,rand,til_fix=False)
-data.save_neighbour_distr(history,'EPS_1_test')
+history = lib.run_simulation(simulation,l,timestep,timend,rand,til_fix=False,save_areas=True)

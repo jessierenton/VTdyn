e54df606efc91bab1b04820959e67281439e722d
Jessie Renton <jsr.renton@gmail.com>
2019-05-24 13:53:46 +0100

    add stress dependent proliferation to library and ability to run single and parallel simulations with variable parameters

--- libs/stress_dep_lib.py
+++ libs/stress_dep_lib.py
@@ -41,7 +41,7 @@ def death_function_poisson(n,rand,T_D=T_D):
 def cycle_function_uniform(n,rand,T_G1=T_G1,T_other=T_other):
     return rand.rand(n)*T_G1*2 + T_other
 
-def simulation_age_dependent(tissue,dt,N_steps,stepsize,rand,til_fix=False,progress_on=False,store_dead=False,save_events=False,T_D=T_D,**kwargs):
+def simulation_constant_pop_size(tissue,dt,N_steps,stepsize,rand,til_fix=False,progress_on=False,store_dead=False,save_events=False,T_D=T_D,**kwargs):
     step = 0.
     complete = False
     properties = tissue.properties
@@ -55,28 +55,54 @@ def simulation_age_dependent(tissue,dt,N_steps,stepsize,rand,til_fix=False,progr
         births = np.where(properties['cycle_length']<=tissue.age)[0]
         if len(births)>0:
             for mother in births:
-                tissue.add_daughter_cells(mother,rand,{'cycle_length':cycle_function_uniform(2,rand),'age_of_death':death_function_poisson(2,rand,T_D=T_D)})
-            tissue.remove(births,np.ones(len(births),dtype=bool))
-        deaths = np.where(properties['age_of_death']<=tissue.age)[0]
-        if len(deaths)>0:
-            tissue.remove(deaths,np.zeros(len(deaths),dtype=bool))
+                tissue.add_daughter_cells(mother,rand,{'cycle_length':cycle_function_uniform(2,rand)})
+            tissue.remove(births,True)
+            deaths = rand.randint(0,N-1,len(births))
+            tissue.remove(deaths,False)
         tissue.update(dt)
         if til_fix: complete = (1 not in tissue.properties['type'] or 0 not in tissue.properties['type']) and step%stepsize==0 
         if not save_events or (len(births)!=0 or len(deaths)!=0): yield tissue 
         else: yield None
-    
-def run_simulation(simulation,N,timestep,timend,rand,init_time=10.,til_fix=False,mutant_num=1,save_areas=False,store_dead=False,tissue=None,force=None,save_events=False,T_D=T_D,**kwargs):
+
+def simulation_stress_dependent(tissue,dt,N_steps,stepsize,rand,til_fix=False,progress_on=False,store_dead=False,save_events=False,T_D=T_D,stress_threshold=np.inf,**kwargs):
+    step = 0.
+    complete = False
+    properties = tissue.properties
+    mesh = tissue.mesh
+    while not til_fix or not complete:
+        if progress_on: 
+            print_progress(step,N_steps)
+            step += 1
+        N=len(tissue)
+        mesh.move_all(tissue.dr(dt))
+        births = np.where(properties['cycle_length']<=tissue.age)[0]
+        births = [mother for mother in births if tissue.cell_stress(mother) < stress_threshold]
+        if len(births)>0:
+            for mother in births:
+                tissue.add_daughter_cells(mother,rand,{'cycle_length':cycle_function_uniform(2,rand),'age_of_death':death_function_poisson(2,rand,T_D=T_D)})
+            tissue.remove(births,True)
+        deaths = np.where(properties['age_of_death']<=tissue.age)[0]
+        if len(deaths)>0:
+            tissue.remove(deaths,False)
+        tissue.update(dt)
+        if til_fix: complete = (1 not in tissue.properties['type'] or 0 not in tissue.properties['type']) and step%stepsize==0 
+        if not save_events or (len(births)!=0 or len(deaths)!=0): yield tissue 
+        else: yield None
+
+def run_simulation(simulation,N,timestep,timend,rand,init_time=10.,til_fix=False,mutant_num=1,ancestors=None,save_areas=False,store_dead=False,tissue=None,force=None,save_events=False,T_D=T_D,stress_threshold=np.inf,**kwargs):
     if tissue is None:
         if force is None: force = BasicSpringForceNoGrowth()
         tissue = init.init_tissue_torus(N,N,0.01,force,rand,save_areas=save_areas,store_dead=store_dead)
-        tissue.properties['type'] = np.zeros(N*N,dtype=int)
         tissue.properties['cycle_length'] = cycle_function_uniform(N*N,rand)
         tissue.age = tissue.properties['cycle_length']*rand.rand(N*N) #initialise cell ages at random point in cell cycle
-        tissue.properties['age_of_death'] = death_function_poisson(N*N,rand,T_D=T_D)+tissue.age #add age of death to initial cell age 
         if init_time is not None: 
-            tissue = run_save_final(tissue, simulation(tissue,dt,init_time/dt,timestep/dt,rand,til_fix=False,store_dead=store_dead,T_D=T_D,**kwargs),init_time/dt)
+            tissue = run_save_final(simulation_constant_pop_size(tissue,dt,init_time/dt,timestep/dt,rand,til_fix=False,store_dead=store_dead,T_D=T_D,**kwargs),init_time/dt)
             tissue.reset(reset_age=False)
-        tissue.properties['type'][rand.choice(N*N,size=mutant_num,replace=False)]=1
+        if mutant_num is not None:
+            tissue.properties['type'] = np.zeros(N*N,dtype=int)
+            tissue.properties['type'][rand.choice(N*N,size=mutant_num,replace=False)]=1
+        if ancestors is not None: tissue.properties['ancestor'] = np.arange(N*N,dtype=int)
+        tissue.properties['age_of_death'] = death_function_poisson(N*N,rand,T_D=T_D)+tissue.age #add age of death to initial cell age 
     if save_events: history = run_save_events(tissue, simulation(tissue,dt,timend/dt,timestep/dt,rand,til_fix=til_fix,store_dead=store_dead,save_events=save_events,T_D=T_D,**kwargs),timend/dt)
-    else: history = run(tissue, simulation(tissue,dt,timend/dt,timestep/dt,rand,til_fix=til_fix,store_dead=store_dead,T_D=T_D,**kwargs),timend/dt,timestep/dt)
+    else: history = run(tissue, simulation(tissue,dt,timend/dt,timestep/dt,rand,til_fix=til_fix,store_dead=store_dead,T_D=T_D,stress_threshold=stress_threshold,**kwargs),timend/dt,timestep/dt)
     return history
--- run_stress_dep.py
+++ run_stress_dep.py
@@ -1,23 +1,60 @@
 import numpy as np
+import multiprocessing as mp
+from functools import partial
+from itertools import repeat
 import libs.stress_dep_lib as lib #library for simulation routines
 import libs.data as data
-import libs.plot as vplt #plotting library
 from structure.global_constants import *
 import structure.initialisation as init
 from structure.cell import Tissue, BasicSpringForceNoGrowth
 import sys
 from optparse import OptionParser
 
+def run_single(i,stress_threshold,T_D,outdir=None,seed=None,return_value="h"):
+    rand = np.random.RandomState(seed)
+    history = lib.run_simulation(simulation,l,timestep,timend,rand,progress_on=options.progress_on,til_fix=options.til_fix,
+                init_time=None,save_areas=False,store_dead=True,save_events=options.save_events,T_D=T_D,ancestors=options.ancestors,
+                stress_threshold=stress_threshold)
+    if outdir is not None:
+        data.save_stress(history,outdir,index=i)
+        data.save_N_cell(history,outdir,index=i)
+        data.save_info(history,outdir,index=i,**{"stress threshold":(stress_threshold,"%.1f"),
+            "cell cycle": ("delayed uniform (low=%d, high=%d)"%(T_other,T_other+T_G1*2),"%s"), 
+            "cell death": ("poisson (mean=%.1f)"%T_D,"%s")})
+    if return_value == "h": return history
+
+def run_single_for_parallel(args):
+    return run_single(*args)
+
+def multiple_runs(repeats,parameters,outdir=None):  
+    num_pvals = len(parameters)
+    if outdir[-1]!='/': outdir+='/'
+    pool = mp.Pool(mp.cpu_count(),maxtasksperchild=1000)     
+    args = [(i,stress_threshold,T_D,outdir+"/Td%.1f_st%.1f"%(T_D,stress_threshold)) 
+                for stress_threshold,T_D in parameters for i in range(repeats)]
+    [f for f in pool.imap(run_single_for_parallel,args)]
+    
 parser = OptionParser()
-parser.set_defaults(save_events=False,progress_on=False,til_fix=False,seed=None)
+parser.set_defaults(save_events=False,progress_on=False,til_fix=False,seed=None,ancestors=False,outdir=None,
+                    repeats=None,parameter_file=None,stress_threshold=10.)
 parser.add_option("-e","--events", action="store_true", dest="save_events",
                     help="store tissue every time a birth/death event occurs")
 parser.add_option("-p","--progress", action="store_true", dest="progress_on",
                     help="show progress bar")
 parser.add_option("-f","--fixation", action="store_true",dest="til_fix", 
                     help="stop simulation after mutants have fixed or died out")
-parser.add_option("-s","--seed", type="int",dest="seed", metavar="SEED",
-                    help="supply seed for random state")
+parser.add_option("-s","--seed", type="int", dest="seed", metavar="SEED",
+                    help="supply seed for random state")                   
+parser.add_option("-a","--ancestors", action="store_true", dest="ancestors",
+                    help="track ancestry of cells")  
+parser.add_option("-d","--data", type="str", dest="outdir",metavar="DIRECTORY",
+                    help="save data for simulation in given directory")                
+parser.add_option("-t","--threshold",type="float",dest="stress_threshold",metavar="THRESHOLD",
+                    help="define the stress threshold above which proliferation does not occur")
+parser.add_option("-m","--multiple", type="int", dest="repeats",metavar="REPEATS",
+                    help="run given number of simulations in parallel")
+parser.add_option("-P","--parameters",type="str", dest="parameter_file",metavar="FILE",
+                    help="provide parameter file for running multiple simulations")
                     
 (options,args) = parser.parse_args()
 
@@ -25,14 +62,13 @@ parser.add_option("-s","--seed", type="int",dest="seed", metavar="SEED",
 
 l = 10 # population size N=l*l
 timend = float(args[0]) # simulation time (hours)
-timestep = .1 # time intervals to save simulation history
-T_D = 17.
+timestep = 0.5 # time intervals to save simulation history
+T_D = 20.
 
-rand = np.random.RandomState(options.seed)
-
-
-simulation = lib.simulation_age_dependent
-
-history = lib.run_simulation(simulation,l,timestep,timend,rand,progress_on=options.progress_on,til_fix=options.til_fix,
-            init_time=None,save_areas=True,store_dead=True,save_events=options.save_events,T_D=T_D)
-                
+simulation = lib.simulation_stress_dependent
+if options.repeats is None: 
+    history = run_single(0,options.stress_threshold,T_D,outdir=options.outdir,seed=options.seed,return_value="h")
+else: 
+    if options.parameter_file is not None: parameters = np.loadtxt(options.parameter_file)
+    else: parameters = [(options.stress_threshold,T_D)]
+    multiple_runs(options.repeats,parameters,outdir=options.outdir)

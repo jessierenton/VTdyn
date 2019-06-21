78b430dbccf8af0ea9a8a31dc6f43b9198cd02c9
Jessie Renton <jsr.renton@gmail.com>
2019-06-21 14:02:53 +0100

    add no stress dependence simulation and make minor changes to "save_events" simulations

--- libs/stress_dep_lib.py
+++ libs/stress_dep_lib.py
@@ -26,7 +26,7 @@ def run(tissue_original,simulation,N_step,skip):
     return [tissue_original.copy()]+[tissue.copy() for tissue in itertools.islice(simulation,skip-1,N_step,skip)]
 
 def run_save_events(tissue_original,simulation,N_step):
-    return [tissue_original.copy()]+[tissue.copy() for tissue in itertools.islice(simulation,N_step) if tissue is not None]
+    return [tissue_original.copy()]+[tissue.copy() for tissue in itertools.islice(simulation,N_step)]
 
 def run_save_final(simulation,N_step):
     return next(itertools.islice(simulation,N_step,None))
@@ -40,6 +40,35 @@ def death_function_poisson(n,rand,T_D=T_D):
 
 def cycle_function_uniform(n,rand,T_G1=T_G1,T_other=T_other):
     return rand.rand(n)*T_G1*2 + T_other
+
+def simulation_no_stress_dependence(tissue,dt,N_steps,stepsize,rand,til_fix=False,progress_on=False,store_dead=False,save_events=False,T_D=T_D,N_limit=np.inf,**kwargs):  
+    step = 0.
+    complete = False
+    properties = tissue.properties
+    mesh = tissue.mesh
+    while not til_fix or not complete:
+        if progress_on: 
+            print_progress(step,N_steps)
+            step += 1
+        N=len(tissue)
+        if N <10 or N>=N_limit: 
+            break
+        mesh.move_all(tissue.dr(dt))
+        births = np.where(properties['cycle_length']<=tissue.age)[0]
+        if len(births)>0:
+            for mother in births:
+                daughter_properties = {'cycle_length':cycle_function_uniform(2,rand)}
+                if T_D is not None: daughter_properties['age_of_death'] = death_function_poisson(2,rand,T_D=T_D)
+                tissue.add_daughter_cells(mother,rand,daughter_properties)
+            tissue.remove(births,True)
+        if T_D is not None:        	
+            deaths = np.where(properties['age_of_death']<=tissue.age)[0]
+            if len(deaths)>0:
+            	tissue.remove(deaths,False)
+        tissue.update(dt)
+        if til_fix: complete = (1 not in tissue.properties['type'] or 0 not in tissue.properties['type']) and step%stepsize==0 
+        if not save_events or (len(births)!=0 or len(deaths)!=0): 
+            yield tissue 
 
 def simulation_constant_pop_size(tissue,dt,N_steps,stepsize,rand,til_fix=False,progress_on=False,store_dead=False,save_events=False,T_D=T_D,**kwargs):
     step = 0.
@@ -61,8 +90,8 @@ def simulation_constant_pop_size(tissue,dt,N_steps,stepsize,rand,til_fix=False,p
             tissue.remove(deaths,False)
         tissue.update(dt)
         if til_fix: complete = (1 not in tissue.properties['type'] or 0 not in tissue.properties['type']) and step%stepsize==0 
-        if not save_events or (len(births)!=0 or len(deaths)!=0): yield tissue 
-        else: yield None
+        if not save_events or (len(births)!=0 or len(deaths)!=0): 
+            yield tissue 
 
 def simulation_stress_dependent(tissue,dt,N_steps,stepsize,rand,til_fix=False,progress_on=False,store_dead=False,save_events=False,T_D=T_D,stress_threshold=np.inf,N_limit=np.inf,**kwargs):
     step = 0.
@@ -81,11 +110,14 @@ def simulation_stress_dependent(tissue,dt,N_steps,stepsize,rand,til_fix=False,pr
         births = [mother for mother in births if tissue.cell_stress(mother) < stress_threshold]
         if len(births)>0:
             for mother in births:
-                tissue.add_daughter_cells(mother,rand,{'cycle_length':cycle_function_uniform(2,rand),'age_of_death':death_function_poisson(2,rand,T_D=T_D)})
+                daughter_properties = {'cycle_length':cycle_function_uniform(2,rand)}
+                if T_D is not None: daughter_properties['age_of_death'] = death_function_poisson(2,rand,T_D=T_D)
+                tissue.add_daughter_cells(mother,rand,daughter_properties)
             tissue.remove(births,True)
-        deaths = np.where(properties['age_of_death']<=tissue.age)[0]
-        if len(deaths)>0:
-            tissue.remove(deaths,False)
+        if T_D is not None:        	
+            deaths = np.where(properties['age_of_death']<=tissue.age)[0]
+            if len(deaths)>0:
+            	tissue.remove(deaths,False)
         tissue.update(dt)
         if til_fix: complete = (1 not in tissue.properties['type'] or 0 not in tissue.properties['type']) and step%stepsize==0 
         if not save_events or (len(births)!=0 or len(deaths)!=0): 
@@ -105,7 +137,7 @@ def run_simulation(simulation,N,timestep,timend,rand,init_time=10.,til_fix=False
             tissue.properties['type'] = np.zeros(N*N,dtype=int)
             tissue.properties['type'][rand.choice(N*N,size=mutant_num,replace=False)]=1
         if ancestors is not None: tissue.properties['ancestor'] = np.arange(N*N,dtype=int)
-        tissue.properties['age_of_death'] = death_function_poisson(N*N,rand,T_D=T_D)+tissue.age #add age of death to initial cell age 
+        if T_D is not None: tissue.properties['age_of_death'] = death_function_poisson(N*N,rand,T_D=T_D)+tissue.age #add age of death to initial cell age 
     if save_events: history = run_save_events(tissue, simulation(tissue,dt,timend/dt,timestep/dt,rand,til_fix=til_fix,store_dead=store_dead,save_events=save_events,T_D=T_D,stress_threshold=stress_threshold,N_limit=N_limit,**kwargs),timend/dt)
     else: history = run(tissue, simulation(tissue,dt,timend/dt,timestep/dt,rand,til_fix=til_fix,store_dead=store_dead,T_D=T_D,stress_threshold=stress_threshold,N_limit=N_limit,**kwargs),timend/dt,timestep/dt)
     return history

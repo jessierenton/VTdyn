685d822e3b37be806123bd5ddaa04df495ad754a
Jessie Renton <jsr.renton@gmail.com>
2019-08-23 10:57:17 +0100

    add routines to data class for returning various data rather than saving

--- libs/data.py
+++ libs/data.py
@@ -58,7 +58,7 @@ def save_neighbour_distr(history,outdir,index=0):
     if not os.path.exists(outdir): # if the folder doesn't exist create it
          os.makedirs(outdir)
     wfilename = '%s/%s_%03d'%(outdir,'neigh_distr',index) 
-    np.savetxt(wfilename,[np.bincount([len(tissue.mesh.neighbours[i]) for i in range(len(tissue))],minlength=18) for tissue in history],fmt=(['%d']*18))
+    np.savetxt(wfilename,neighbour_distribution(history),fmt=(['%d']*18))
 
 
 def save_N_cell(history,outdir,index=0):
@@ -66,14 +66,40 @@ def save_N_cell(history,outdir,index=0):
     if not os.path.exists(outdir): # if the folder doesn't exist create it
          os.makedirs(outdir)
     wfilename = '%s/%s_%03d'%(outdir,'N_cell',index)  
-    np.savetxt(wfilename,[len(tissue) for tissue in history],fmt=('%d'))
+    np.savetxt(wfilename,population_size(history),fmt=('%d'))
 
 def save_N_mutant(history,outdir,index=0):
     """saves number of mutants in each tissue given by 'type' property"""
     if not os.path.exists(outdir): # if the folder doesn't exist create it
          os.makedirs(outdir)
     wfilename = '%s/%s_%03d'%(outdir,'N_mutant',index)  
-    np.savetxt(wfilename,[sum(tissue.properties['type']) for tissue in history],fmt=('%d'))
+    np.savetxt(wfilename,number_mutants(history),fmt=('%d'))
+
+def number_mutants(history):
+    return [sum(tissue.properties['type']) for tissue in history]
+
+def population_size(history):
+    return [len(tissue) for tissue in history]
+
+def neighbour_distribution(history):
+    return [np.bincount([len(tissue.mesh.neighbours[i]) for i in range(len(tissue))],minlength=18) for tissue in history]
+
+def cell_cycle_lengths(history,start_time=0.0):
+    tissue = history[-1]
+    divided_cell_info = np.array(tissue.divided_cells).T
+    cycle_lengths = (divided_cell_info[1])[divided_cell_info[2]>start_time]
+    return cycle_lengths
+
+def extrusion_ages(history,start_time=0.0):
+    tissue = history[-1]
+    extruded_cell_info = np.array(tissue.extruded_cells).T
+    extrusion_ages = (extruded_cell_info[1])[extruded_cell_info[2]>start_time]
+    return extrusion_ages
+
+def cycle_phase_proportion(history,phase):
+    return [sum(tissue.properties['cycle_phase']==phase)/float(len(tissue)) for tissue in history] 
+    
+
 
 @memoize
 def get_local_density(mesh):
@@ -88,62 +114,6 @@ def save_info(history,outdir,index=0,**kwargs):
         f.write('initial population size = %d \n'%len(history[0]))
         for key,(val,fmt) in kwargs.iteritems():
             f.write(key+' = '+fmt%val+'\n')
-        
-# def save_division_extrusion_ages(history,outdir,index=0):
-#     dt = history[1].time-history[0].time
-#     division_ages = []
-#     extrusion_ages = []
-#     for current_tissue,previous_tissue in zip(history[:-1],history[1:]):
-#         previous_cells = previous_tissue.cell_ids
-#         for cell in previous_cells:
-#             if cell in current_tissue.extruded_cells:
-#                 extrusion_ages.append(cell.age+dt)
-#                 break
-#             elif cell in current_tissue.divided_cells:
-#                 division_ages.append(cell.age+dt)
-#                 break
-#     f_e,f_d = '%s/%s_%03d'%(outdir,'extrusion_ages',index),'%s/%s_%03d'%(outdir,'division_ages',index)
-
-# def get_cell_history(history,cell_id,area=False,density=False):
-#     """generate a history for a given cell id with area at each timestep, age at each timestep and
-#     fate (reproduction=1 or death=0)"""
-#     cell_history = {'age':[],'fate':None,'mother':None}
-#     if area: cell_history['area']=[]
-#     if density: cell_history['density']=[]
-#     for tissue in history:
-#         if cell_id not in tissue.cell_ids:
-#             if cell_id in tissue.mother:
-#                 cell_history['fate'] = 1
-#                 break
-#             elif len(cell_history['age']) > 0:
-#                 cell_history['fate'] = 0
-#                 break
-#         elif cell_id in tissue.cell_ids:
-#             if cell_history['mother'] is None:
-#                 cell_history['mother'] = tissue.mother[np.where(tissue.cell_ids==cell_id)[0][0]]
-#             mesh_id = np.where(tissue.cell_ids == cell_id)[0][0]
-#             if area: cell_history['area'].append(tissue.mesh.areas[mesh_id])
-#             cell_history['age'].append(tissue.age[mesh_id])
-#             if density:
-#                 cell_history['density'].append(get_local_density(tissue.mesh)[mesh_id])
-#     return cell_history
-#
-# def get_cell_histories(history,start=0,area=False,density=False):
-#     """generate history for all cells (see above get_cell_history)"""
-#     return [ch for ch in (get_cell_history(history[start:],i,area,density) for i in range(max(history[-1].cell_ids)+1))]
-
-def save_age_of_death(history,outdir,index=0):
-    """save cell lifetimes for each cell in history"""
-    if not os.path.exists(outdir): # if the folder doesn't exist create it
-         os.makedirs(outdir)
-    cell_histories = np.array(get_cell_histories(history))
-    has_fate = np.array([h['fate'] is not None for h in cell_histories])
-    cell_histories = cell_histories[has_fate]
-    fates = np.array([h['fate'] for h in cell_histories],dtype=bool)
-    final_age_d = [cell['age'][-1] for cell in cell_histories[fates]]
-    final_age_a = [cell['age'][-1] for cell in cell_histories[~fates]]
-    np.savetxt('%s/division_age_%03d'%(outdir,index),final_age_d)
-    np.savetxt('%s/apoptosis_age_%03d'%(outdir,index),final_age_a)
     
 def save_ages(history,outdir,index=0):
     """saves all cell ages for each tissue in history"""
@@ -198,7 +168,7 @@ def _calc_var_to_mean(width,height,centres,s):
 def calc_density(width,height,centres,s):
     range_=((-width/2.,width/2),(-height/2,height/2))
     return np.histogram2d(centres[:,0],centres[:,1],s,range_)[0]
-        
+  
 def save_all(history,outdir,index=0):
     save_N_cell(history,outdir,index)
     save_N_mutant(history,outdir,index)

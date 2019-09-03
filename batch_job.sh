10dc5af90ed906f12dee62e070b79c3390dc1b54
Jessie Renton <jsr.renton@gmail.com>
2019-09-03 11:35:24 +0100

    correct run files for CIP simulations with fixed cell cycle rates and fixed domain size

--- batch_job.sh
+++ batch_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=60
-#SBATCH --array=0-2
+#SBATCH --array=0-3
 #SBATCH --nodes=1
 
 source /clusternfs/jrenton/anaconda2/my_anaconda.sh
-python run_CIP_parallel.py param${SLURM_ARRAY_TASK_ID}.txt
+python run_CIP_parallel.py param${SLURM_ARRAY_TASK_ID}.txt 3

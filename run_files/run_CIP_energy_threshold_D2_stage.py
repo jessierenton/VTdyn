24e9448df36b278746a8ac857058d12636244618
Jessie Renton <jsr.renton@gmail.com>
2019-11-21 12:55:45 +0000

    add various run files

---
+++ run_files/run_CIP_energy_threshold_D2_stage.py
@@ -0,0 +1,26 @@
+import numpy as np
+import libs.contact_inhibition_lib as lib #library for simulation routines
+import libs.data as data
+import libs.plot as vplt #plotting library
+from structure.global_constants import *
+import structure.initialisation as init
+from structure.cell import Tissue, BasicSpringForceNoGrowth
+
+"""run a single voronoi tessellation model simulation"""
+
+l = 10 # population size N=l*l
+timend = 10. # simulation time (hours)
+timestep = 1. # time intervals to save simulation history
+
+rand = np.random.RandomState()
+
+simulation = lib.simulation_contact_inhibition  #simulation routine imported from lib
+CIP_parameters = {'threshold':100.}
+rates = (0.04,0.5,0.1) #deaths_rate,G_to_S_rate,S_to_div_rate
+domain_size_multiplier=1.5
+
+history = lib.run_simulation(simulation,l,timestep,timend,rand,progress_on=True,
+            init_time=None,til_fix=False,save_areas=False,cycle_phase=True,
+            return_events=False,save_cell_histories=True,domain_size_multiplier=domain_size_multiplier,
+            CIP_parameters=CIP_parameters,rates=rates)
+

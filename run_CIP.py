2f5ea3ce289983a42c72085fe2949c089e726e08
Jessie Renton <jsr.renton@gmail.com>
2020-01-07 11:39:29 +0000

    params for pop_size 100

--- run_CIP.py
+++ run_CIP.py
@@ -9,15 +9,16 @@ from structure.cell import Tissue, BasicSpringForceNoGrowth
 """run a single voronoi tessellation model simulation"""
 
 l = 10 # population size N=l*l
-timend = 300. # simulation time (hours)
+timend = 1000. # simulation time (hours)
 timestep = 96. # time intervals to save simulation history
 init_time = None
 
 rand = np.random.RandomState(128) 
 simulation = lib.simulation_contact_inhibition_area_dependent  #simulation routine imported from lib
 threshold_area_fraction=1.0
+death_to_birth_rate_ratio = 0.1
 DEATH_RATE = 0.25/24 #death rate: 0.25 deaths per day per cell
-rates = (DEATH_RATE,DEATH_RATE/0.1) #death_rate,division_rate
+rates = (DEATH_RATE,DEATH_RATE/death_to_birth_rate_ratio) #death_rate,division_rate
 domain_size_multiplier=0.948836
 
 DELTA = 0.025
@@ -25,7 +26,7 @@ game_constants = (1.,1.)
 game = lib.prisoners_dilemma_averaged
 
 history = lib.run_simulation(simulation,l,timestep,timend,rand,progress_on=True,
-            init_time=init_time,til_fix=True,save_areas=True,cycle_phase=None,
+            init_time=init_time,til_fix=False,save_areas=True,cycle_phase=None,
             return_events=False,save_cell_histories=False,domain_size_multiplier=domain_size_multiplier,
-            DELTA=DELTA,game=game,game_constants=game_constants,mutant_num=1,
+            DELTA=DELTA,game=game,game_constants=game_constants,mutant_num=100,
             rates=rates,threshold_area_fraction=threshold_area_fraction)

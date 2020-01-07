65f243041a0e0ad248c3259335e34111325128ba
Jessie Renton <jsr.renton@gmail.com>
2020-01-07 15:32:23 +0000

    add bash script for running single sims not on hpc

---
+++ single_CIP_pd.sh
@@ -0,0 +1,10 @@
+PARAMFILE=pd_params2/p$1
+number=$(($2+1))
+p1=`(sed -n "$number"p $PARAMFILE) | awk '{print $1}'`
+p2=`(sed -n "$number"p $PARAMFILE) | awk '{print $2}'`
+p3=`(sed -n "$number"p $PARAMFILE) | awk '{print $3}'`
+p4=`(sed -n "$number"p $PARAMFILE) | awk '{print $4}'`
+
+source /local/jrenton/anaconda2/my_anaconda.sh
+
+python run_CIP_parallel_pd.py $p1 $p2 $p3 $p4 11 

3681073a6d6cc8a886bc86eaa456babbee72d765
Jessie Renton <jsr.renton@gmail.com>
2020-04-24 15:18:37 +0100

    rearrange file structure- move data files out of rep and move run files into own folders

--- CIP_data/CIP_data_fixed_cc_rates/code/batch_job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=60
-#SBATCH --array=0-2
-#SBATCH --nodes=1
-
-source /clusternfs/jrenton/anaconda2/my_anaconda.sh
-python run_CIP_parallel.py param${SLURM_ARRAY_TASK_ID}.txt

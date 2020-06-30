d525905597d81c694d52e76c016f3a5bbce6ad21
Jessie Renton <jsr.renton@gmail.com>
2020-06-30 09:47:45 +0100

    edit run_CIP.py

--- run_CIP.py
+++ run_CIP.py
@@ -9,11 +9,12 @@ from structure.cell import Tissue, BasicSpringForceNoGrowth
 PROGRESS_ON = True
 SAVE_CELL_HISTORIES = False
 TIL_FIX = False
+DEATH_RATE = 0.25/24 #death rate: 0.25 deaths per day per cell
 
 """run a single voronoi tessellation model simulation"""
 
 l = 10 # population size N=l*l
-timend = 24*50 # simulation time (hours)
+timend = 24 # simulation time (hours)
 timestep = 6 # time intervals to save simulation history
 init_time = 96
 
@@ -22,18 +23,18 @@ rand = np.random.RandomState()
 simulation = lib.simulation_contact_inhibition_area_dependent
 threshold_area_fraction,death_to_birth_rate_ratio,domain_size_multiplier = 1.200000,0.100000,1.030535
 # threshold_area_fraction,death_to_birth_rate_ratio,domain_size_multiplier=0.800000,0.100000,0.859628
-DEATH_RATE = 0.25/24 #death rate: 0.25 deaths per day per cell
-rates = (DEATH_RATE,DEATH_RATE/death_to_birth_rate_ratio) #death_rate,division_rate
+rates = (DEATH_RATE,DEATH_RATE/death_to_birth_rate_ratio*1.125) #death_rate,division_rate
 
 
 DELTA = 0.025
-game_constants = (1.,1.)
-game = None
+game_constants = (4.,1.)
+game = lib.prisoners_dilemma_averaged
+initial_mutants = 1
 
 history = lib.run_simulation(simulation,l,timestep,timend,rand,progress_on=PROGRESS_ON,
             init_time=init_time,til_fix=TIL_FIX,save_areas=True,cycle_phase=None,
             return_events=False,save_cell_histories=SAVE_CELL_HISTORIES,domain_size_multiplier=domain_size_multiplier,
-            DELTA=DELTA,game=game,game_constants=game_constants,mutant_num=0,ancestors=True,
+            DELTA=DELTA,game=game,game_constants=game_constants,mutant_num=initial_mutants,mutant_type=1,ancestors=True,
             rates=rates,threshold_area_fraction=threshold_area_fraction)
 
 

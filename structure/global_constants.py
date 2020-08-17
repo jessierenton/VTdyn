52279c69ca6b10bfcf465b0f096ce7317058c1a4
Jessie Renton <jsr.renton@gmail.com>
2020-08-17 09:59:28 +0100

    use original global_params for pgg (including T_M)

--- structure/global_constants.py
+++ structure/global_constants.py
@@ -1,19 +1,22 @@
 global T_D,T_M,L0,EPS,MU,ETA,dt
 
-T_M = None
-T_D = 24./0.25
-# T_D = 12.
-
 L0 = 1.0
 A0 = 3**0.5/2.
 EPS = 0.05
 
-# Osborne 2017 params (scaled)
-MU = 6.25
-ETA = 1.0
-dt = 0.04 #hours
-
-# # Osborne 2017 params (original)
-# MU = 50
+# params for density dependent model
+# T_M = None
+# T_D = 24./0.25
+# # Osborne 2017 params (scaled)
+# MU = 6.25
 # ETA = 1.0
-# dt = 0.005 #hours
+# dt = 0.04 #hours
+
+
+# params for two-player games and pgg (original decoupled model)
+T_M = 1.
+T_D = 12.
+# # Osborne 2017 params (original)
+MU = 50
+ETA = 1.0
+dt = 0.005 #hours
